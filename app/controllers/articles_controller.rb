e503b5f992447dd0982f8d3fe0862aef6eef08d3
sangeeth-review <sangeeth@sapaad.com>
2020-09-02 21:05:39 +0530

    add Create, read , delete facility

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,44 @@
 class ArticlesController < ApplicationController
 
- def show
- 	
+def show
+
  	@article = Article.find(params[:id])
  	 
- end
+end
 
 
+def create 
+	@article = Article.new(params.require(:article).permit(:title, :description))
+
+#	render plain: @article.inspect
+	if @article.save
+		redirect_to article_path(@article)
+
+	else
+		render 'new'
+	end 
+
+end
+
+def new
+	@article = Article.new
+end
+
+def index
+	@articles = Article.all
+end
+
+def destroy
+	@article = Article.find(params[:id])
+	@article.destroy
+	redirect_to articles_path
+end
+
+def update
+	@article  = Article.find(params[:id])
+	@article = Article.new(params.require(:article).permit(:title, :description))
+	@article.save
+	redirect_to articles_path
+end
 
 end
