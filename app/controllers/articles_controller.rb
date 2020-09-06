a6606b24b83a472b15cf8ae8b51cd69f2d7a9b3b
sangeeth-review <sangeeth@sapaad.com>
2020-09-06 15:11:27 +0530

    add update option

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,9 +12,12 @@ def create
 
 #	render plain: @article.inspect
 	if @article.save
+		flash[:notice] = "Article was created succesfully."
 		redirect_to article_path(@article)
 
+
 	else
+		flash[:notice] = "Article was not created succesfully."
 		render 'new'
 	end 
 
@@ -34,11 +37,19 @@ def destroy
 	redirect_to articles_path
 end
 
+def edit
+	@article = Article.find(params[:id])
+end
+
 def update
-	@article  = Article.find(params[:id])
-	@article = Article.new(params.require(:article).permit(:title, :description))
-	@article.save
-	redirect_to articles_path
+	@article = Article.find(params[:id])
+#	@article = Article.new
+	if @article.update(params.require(:article).permit(:title, :description))
+		flash[:notice] = "Article edited successfully."
+		redirect_to @article
+	else
+		render 'edit'
+	end
 end
 
 end
